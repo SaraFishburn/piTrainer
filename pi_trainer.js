c045fa57d743a7e1ebdaccc196332055344886aa
Sara Fishburn <fishburn.sara@gmail.com>
2021-03-21 22:03:01 +1000

    input correct case handled

--- pi_trainer.js
+++ pi_trainer.js
@@ -1,5 +1,41 @@
-// spigot algorithm courtesy of Vladimir Agafonkin https://observablehq.com/@mourner/calculating-pi-digits
-digits = function* digits() {
+const sleep = (time) => new Promise((r) => setTimeout(r, time))
+
+document.addEventListener('DOMContentLoaded', async function() 
+{
+  let input = document.getElementById('input')
+  input.disabled = true
+  let limit = 5
+  let pi = calculatePi(limit)
+  await printPi(pi)
+  input.disabled = false
+  input.focus()
+
+  input.addEventListener('keyup', async function () {
+    if(compareDigits(pi, input)) {
+      console.log('success')
+
+      if(input.value.length === pi.length) {
+        input.style.color = 'green'
+        await sleep(500)
+        input.value = ''
+        input.disabled = true
+        input.style.color = null
+        
+        limit += 1
+        pi = calculatePi(limit)
+        await printPi(pi)
+        input.disabled = false
+        input.focus()
+      }
+    } 
+    else {
+      console.log('fail')
+    }
+  })
+})
+
+// spigot algorithm by Vladimir Agafonkin https://observablehq.com/@mourner/calculating-pi-digits
+digits = function* findDigits() {
   let q = 1n, r = 0n, t = 1n, k = 1n, n = 3n, l = 3n
   while (true) {
     if (q * 4n + r - t < n * t) {
@@ -26,4 +62,23 @@ function calculatePi(num) {
   }
   pi.splice(1, 0, '.')
   return pi
+}
+
+async function printPi(pi) {
+  element = document.getElementById('pi')
+  for(let i = 0; i < pi.length; i++) {
+    element.textContent += pi[i]
+    await sleep(500)
+  }
+  element.textContent = ''
+}
+
+function compareDigits(pi, input) { 
+  let input_array = input.value.split('')
+  let index = input_array.length - 1
+
+  if(pi[index] != input_array[index]) {
+    return false
+  }
+  return true
 }
